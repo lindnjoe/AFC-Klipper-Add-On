29aed408042bc83c483e144767d44d762e85e61f
lindnjoe <32586000+lindnjoe@users.noreply.github.com>
2026-02-20 12:44:01 -0500

    Add support for custom load and unload sequences

--- extras/AFC.py
+++ extras/AFC.py
@@ -1081,6 +1081,9 @@ class afc:
         self.LANE_UNLOAD( cur_lane )
 
     def LANE_UNLOAD(self, cur_lane: AFCLane):
+        if hasattr(cur_lane.unit_obj, 'lane_unload'):
+            return cur_lane.unit_obj.lane_unload(cur_lane)
+        
         cur_hub = cur_lane.hub_obj
 
         self.current_state = State.EJECTING_LANE
@@ -1295,6 +1298,9 @@ class afc:
         :param cur_hub: The hub object associated with the lane.
         :param cur_extruder: The extruder object associated with the lane.
         """
+        # Allow unit to provide custom load sequence
+        if hasattr(cur_lane.unit_obj, 'load_sequence'):
+            return cur_lane.unit_obj.load_sequence(cur_lane, cur_hub, cur_extruder)
         # Placeholder for custom load sequence
         if cur_lane.custom_load_cmd:
             self.logger.info("Running custom load command for lane {}".format(cur_lane.name))
@@ -1565,6 +1571,9 @@ class afc:
         :param cur_hub: The hub object associated with the lane.
         :param cur_extruder: The extruder object associated with the lane.
         """
+        # Allow unit to provide custom unload sequence
+        if hasattr(cur_lane.unit_obj, 'unload_sequence'):
+            return cur_lane.unit_obj.unload_sequence(cur_lane, cur_hub, cur_extruder)
         if cur_lane.custom_unload_cmd:
             self.logger.info("Running custom unload command for lane {}".format(cur_lane.name))
             cur_lane.status = AFCLaneState.TOOL_UNLOADING
