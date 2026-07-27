de9aea804ca3101a8132881645a741597241c679
jimmyjon711 <jcmadill1@gmail.com>
2026-07-26 18:48:54 -0700

    Updating changelog and version

--- extras/AFC.py
+++ extras/AFC.py
@@ -45,7 +45,7 @@ except: raise error(ERROR_STR.format(import_lib="AFC_utils", trace=traceback.for
 try: from extras.AFC_stats import AFCStats
 except: raise error(ERROR_STR.format(import_lib="AFC_stats", trace=traceback.format_exc()))
 
-AFC_VERSION="1.2.0"
+AFC_VERSION="1.2.1"
 
 # Class for holding different states so its clear what all valid states are
 class State(str, Enum):
